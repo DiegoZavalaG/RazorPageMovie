07f7a97be3eb671a4cf56d3439dd1cba1e8bf138
Diego Zavala Guzman <diego_zavala@live.com>
2023-10-28 13:29:38 -0600

    Agregar archivos de proyecto.

---
+++ RazorPageMovie/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Timers;
+
+namespace RazorPageMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        
+        public string? Title { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime RealeseDate { get; set; }
+        public string? Genre { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+        
+    }
+}
